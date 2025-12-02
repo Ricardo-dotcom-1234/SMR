58d9e6bbbc58b44e601a78e522b3607c9b810e7c
Ricardo-dotcom-1234 <rgongut0810@g.educaand.es>
2025-12-02 09:43:19 +0100

    dfgdfhdsf

--- 2SMRA/Aplicaciones Web/Proyecto/Acompañante_java1.js
+++ 2SMRA/Aplicaciones Web/Proyecto/Acompañante_java1.js
@@ -1,3 +1,9 @@
 let titulo = document.querySelector("p#grax");
 titulo.textContent = "Gracias por visitar SpeedLine Motors";
 
+function toggleMenu() {
+    const menu = document.getElementById('menuList');
+    menu.classList.toggle('menu-collapsed');
+    menu.classList.toggle('menu-expanded');
+}
+
